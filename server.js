d8b79d5f7256b7f0174137e56ace065bce9053d2
Emma Olson <emma.olson794@gmail.com>
2022-05-16 12:12:58 -0500

    first db.query request

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,12 +7,27 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root', // your MySQL username
+        password: '', // your MySQL password
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 
 // testing connection
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello World'
     });
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 // Default response for any other request (not found) / catchall route / must go last
