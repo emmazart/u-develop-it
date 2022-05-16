6c565927001afeba755d610dbbff7fba4159a819
Emma Olson <emma.olson794@gmail.com>
2022-05-16 12:24:33 -0500

    write db query statements

--- server.js
+++ server.js
@@ -26,9 +26,38 @@ app.get('/', (req, res) => {
     });
 });
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
+// // get list of all candidates
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// get a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES(?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err){
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 // Default response for any other request (not found) / catchall route / must go last
 app.use((req, res) => {
